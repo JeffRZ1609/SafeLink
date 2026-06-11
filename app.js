bd57098c263ad96b6435678584eac4525d71186f
Jefferson Rojas Zeledon <jeffersonrojaszeledon11@gmail.com>
2026-06-11 13:52:37 -0600

    mejora del funcionamiento y se agrega la deteccion de acortadores

--- app.js
+++ app.js
@@ -7,9 +7,30 @@ boton.addEventListener("click", function () {
 
 
     const regex =
-    /^(https?):\/\/([^\/]+)(\/.*)?$/i;
+        /^(https?):\/\/([^\/]+)(\/.*)?$/i;
 
     const match = enlace.match(regex);
+
+    if (enlace !== "" && !match) {
+
+        resultado.innerHTML =
+            "El enlace no parece valido.<br>"
+
+        return;
+    }
+
+    let positivos = "";
+    let alertas ="";
+
+     const acortadores = [
+        "bit.ly",
+        "tinyurl.com",
+        "t.co",
+        "cutt.ly",
+        "ow.ly",
+        "is.gd"
+    ];
+
 
     if (enlace === "") {
 
@@ -21,24 +42,47 @@ boton.addEventListener("click", function () {
         let dominio = match[2];
         let ruta = match[3] || "/";
 
+        positivos += "✓ Utiliza HTTPS<br>";
+
+        if (acortadores.includes(dominio)) {
+
+            alertas +=
+                "URL acortada detectada.<br>" +
+                "El destino real del enlace está oculto.<br>";
+        }
+
         resultado.innerHTML =
+
             " El enlace parece seguro.<br>" +
             "Utiliza HTTPS, que es un protocolo más seguro para navegar." +
 
             "<br><strong>Protocolo:</strong>" + protocolo +
             "<br><strong>Dominio</strong>" + dominio +
-            "<br><strong>Ruta:</strong>" + ruta;
+            "<br><strong>Ruta:</strong>" + ruta +
+            "<br><br>" + alertas;
 
     } else if (enlace.startsWith("http://")) {
-        
+
         let protocolo = match[1];
         let dominio = match[2];
         let ruta = match[3] || "/";
 
+        alertas +=
+            "Utiliza HTTP en lugar de HTTPS.<br>";
+
+        if (acortadores.includes(dominio)) {
+
+            alertas +=
+                "URL acortada detectada.<br>" +
+                "El destino real del enlace está oculto.<br>";
+        }
+
 
         resultado.innerHTML =
-            " Ten cuidado con este enlace.<br>" +
-            "Utiliza HTTP en lugar de HTTPS." +
+
+            "<strong>Alertas:</strong><br>" +
+            alertas +   
+
 
             "<br><strong>Protocolo:</strong>" + protocolo +
             "<br><strong>Dominio</strong>" + dominio +
