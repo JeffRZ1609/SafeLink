16824efa3a6dae86ecacbc57c07f5adedc2b2126
Jefferson Rojas Zeledon <jeffersonrojaszeledon11@gmail.com>
2026-06-15 14:04:42 -0600

    Actualizacion app.js, implementacion e incio del backend

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 let boton = document.getElementById("btnAnalizar");
 let resultado = document.getElementById("resultado");
 
-boton.addEventListener("click", function () {
+boton.addEventListener("click", async function () {
 
     let enlace = document.getElementById("url").value.trim();
-
 
     const regex =
         /^(https?):\/\/([^\/]+)(\/.*)?$/i;
@@ -14,15 +13,16 @@ boton.addEventListener("click", function () {
     if (enlace !== "" && !match) {
 
         resultado.innerHTML =
-            "El enlace no parece valido.<br>"
+            "El enlace no parece valido.<br>";
 
         return;
     }
 
     let positivos = "";
-    let alertas ="";
+    let alertas = "";
+    let informacionBackend = "";
 
-     const acortadores = [
+    const acortadores = [
         "bit.ly",
         "tinyurl.com",
         "t.co",
@@ -31,7 +31,7 @@ boton.addEventListener("click", function () {
         "is.gd"
     ];
 
-     const regexIP =
+    const regexIP =
         /^(\d{1,3}\.){3}\d{1,3}$/;
 
     const palabrasPhishing = [
@@ -48,9 +48,60 @@ boton.addEventListener("click", function () {
 
     if (enlace === "") {
 
-        resultado.innerHTML = " Por favor, ingrese un enlace.";
+        resultado.innerHTML =
+            "Por favor, ingrese un enlace.";
 
-    } else if (enlace.startsWith("https://")) {
+        return;
+    }
+
+    // CONEXIÓN CON EL BACKEND
+
+    try {
+
+        const respuesta = await fetch("http://localhost:3000/analizar", {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify({
+
+                url: enlace
+
+            })
+
+        });
+
+        const datos = await respuesta.json();
+
+        informacionBackend =
+
+            "<hr>" +
+
+            "<strong>Análisis de la pagina:</strong><br>" +
+
+            "<strong>Título:</strong> " + datos.titulo +
+
+            "<br><strong>Tamaño HTML:</strong> " + datos.longitudHTML;
+
+    }
+
+    catch (error) {
+
+        informacionBackend =
+
+            "<hr>" +
+
+            "No fue posible obtener información desde el servidor.";
+
+    }
+
+
+    if (enlace.startsWith("https://")) {
 
         let protocolo = match[1];
         let dominio = match[2];
@@ -65,35 +116,40 @@ boton.addEventListener("click", function () {
                 "El destino real del enlace está oculto.<br>";
         }
 
-        if (regexIP.test(dominio)){
+        if (regexIP.test(dominio)) {
 
             alertas +=
                 "Se detectó una dirección IP en lugar de un dominio.<br>" +
                 "Algunos sitios maliciosos utilizan direcciones IP para ocultar su verdadera identidad.<br><br>";
         }
 
-        for (let palabra of palabrasPhishing){
+        for (let palabra of palabrasPhishing) {
 
             if (enlace.toLowerCase().includes(palabra)) {
 
                 alertas +=
-                    "El enlace contiene palabras frecuentes utilizada en links para phishing.<br><br>"
+                    "El enlace contiene palabras frecuentes utilizadas en links para phishing.<br><br>";
 
                 break;
             }
 
         }
+
         resultado.innerHTML =
 
-            " El enlace parece seguro.<br>" +
+            "El enlace parece seguro.<br>" +
             "Utiliza HTTPS, que es un protocolo más seguro para navegar." +
 
-            "<br><strong>Protocolo:</strong>" + protocolo +
-            "<br><strong>Dominio:</strong>" + dominio +
-            "<br><strong>Ruta:</strong>" + ruta +
-            "<br><br>" + alertas;
+            "<br><strong>Protocolo:</strong> " + protocolo +
+            "<br><strong>Dominio:</strong> " + dominio +
+            "<br><strong>Ruta:</strong> " + ruta +
+            "<br><br>" + alertas +
 
-    } else if (enlace.startsWith("http://")) {
+            informacionBackend;
+
+    }
+
+    else if (enlace.startsWith("http://")) {
 
         let protocolo = match[1];
         let dominio = match[2];
@@ -109,39 +165,42 @@ boton.addEventListener("click", function () {
                 "El destino real del enlace está oculto.<br>";
         }
 
-                if (regexIP.test(dominio)){
+        if (regexIP.test(dominio)) {
 
             alertas +=
                 "Se detectó una dirección IP en lugar de un dominio.<br>" +
                 "Algunos sitios maliciosos utilizan direcciones IP para ocultar su verdadera identidad.<br><br>";
         }
 
-        for (let palabra of palabrasPhishing){
+        for (let palabra of palabrasPhishing) {
 
             if (enlace.toLowerCase().includes(palabra)) {
 
                 alertas +=
-                    "El enlace contiene palabras frecuentes utilizada en links para phishing.<br><br>"
+                    "El enlace contiene palabras frecuentes utilizadas en links para phishing.<br><br>";
 
                 break;
             }
+
         }
 
         resultado.innerHTML =
 
             "<strong>Alertas:</strong><br>" +
-            alertas +   
+            alertas +
 
+            "<br><strong>Protocolo:</strong> " + protocolo +
+            "<br><strong>Dominio:</strong> " + dominio +
+            "<br><strong>Ruta:</strong> " + ruta +
 
-            "<br><strong>Protocolo:</strong>" + protocolo +
-            "<br><strong>Dominio:</strong>" + dominio +
-            "<br><strong>Ruta:</strong>" + ruta;
+            informacionBackend;
 
+    }
 
-    } else {
+    else {
 
         resultado.innerHTML =
-            " El enlace no parece válido.<br>" +
+            "El enlace no parece válido.<br>" +
             "Verifique que esté escrito correctamente.";
 
     }
--- server.js
+++ server.js
@@ -1,1 +1,71 @@
-//parte de backend//
+//Librerias
+
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const cheerio = require("cheerio");
+
+//Implementacion de Express
+
+const app = express();
+const PORT = 3000;
+
+
+// MNiddlewares
+
+app.use(cors());
+app.use(express.json());
+
+
+// Ruta Principal
+
+app.get("/", (req, res) => {
+
+    res.send("Servidor de SafeLink funcionando correctamente.");
+
+});
+
+
+// Analisis del Analisis
+
+app.post("/analizar", async (req, res) => {
+
+    try {
+
+        const url = req.body.url;
+
+        const respuesta = await axios.get(url);
+
+        const html = respuesta.data;
+
+        const $ = cheerio.load(html);
+
+        const titulo = $("title").text();
+
+        res.json({
+
+            mensaje: "HTML obtenido correctamente.",
+            longitudHTML: html.length,
+            titulo: titulo
+
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+
+            mensaje: "No fue posible acceder a la página."
+
+        });
+
+    }
+
+});
+
+// Servidor
+
+app.listen(PORT, () => {
+
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+
+});
